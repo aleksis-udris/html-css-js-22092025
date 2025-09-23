bb2039369d670682b2e9cfab924d2e4adbd54750
Your Name <you@example.com>
2025-09-23 10:27:17 +0300

    data table page to output data

---
+++ datatable.php
@@ -0,0 +1,39 @@
+<html>
+    <body>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Level</th>
+                    <th>Review</th>
+                </tr>
+            </thead>
+            <tbody>
+        <?php
+            include "database_connection.php";
+
+            $command = "SELECT * FROM reviews";
+
+            $res = $conn->query($command);
+
+
+
+            if ($res->num_rows > 0) {
+                while ($row = $res->fetch_assoc()) {
+                    echo '<tr>';
+                    echo '<td>' . $row["r_id"] . '</td>';
+                    echo '<td>' . $row["name"] . '</td>';
+                    echo '<td>' . $row["email"] . '</td>';
+                    echo '<td>' . $row["level"] . '</td>';
+                    echo '<td>' . $row["review"] . '</td>';
+                    echo '</tr>';
+                }
+            }
+        ?>
+            </tbody>
+        </table>
+
+    </body>
+</html>
